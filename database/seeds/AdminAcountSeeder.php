4fb3625db102484b402baf9cf707f594f0090cec
AranKyun <447245806@qq.com>
2016-03-02 10:25:09 +0800

    Create seeder for creating a default admin

---
+++ database/seeds/AdminAcountSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminAcountSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		if (User::where('tag', 'admin')->count() == 0) {
+			$admin = User::create([
+				'name' => 'admin',
+				'email' => 'root@admin.com',
+				'password' => bcrypt('root'),
+				'tag' => 'admin',
+			]);
+			$this->command->info('Creating a default admin acount successful!');
+		} else {
+			$this->command->info('Admin acount is already existing in the user table!');
+		}
+	}
+}
