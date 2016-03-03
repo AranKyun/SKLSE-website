6b75ecfb7727b45f76e1e299c8964a9a14e81bf0
AranKyun <447245806@qq.com>
2016-03-03 09:26:30 +0800

    Finish the list of blogs

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,7 +2,7 @@
 @extends('navbar')
 
 @section('title')
-个人博客
+个人空间列表
 @endsection
 
 @section('extra-link')
@@ -10,12 +10,40 @@
 @endsection
 
 @section('content')
+<div class="container">
+	<div class="row" style="margin-top: 20px;">
+		<div class="col-sm-offset-1 div col-sm-6">
+			<p class="ltittle">个人空间列表</p>
+		</div>
+	</div>
+	<div class="row">
+	<?php use App\Blog;?>
+	@foreach($users as $user)
+	<?php $blog = Blog::where('user_id', $user->id)->first();?>
+	<div class="col-sm-4">
 
-        <div style='height:20px;'></div>
-        @foreach($users as $user)
-		<div class="username" style='height:50px;background-color:white;width:80%;margin-left:25%;margin-bottom:3%; border-left: 5px solid #003262;'><h3  style='padding:10px;margin:5px;'><span><b>姓名：</b></span><span><b><a style="text-decoration: none;" href="{{URL('/blog/'.$user->id.'/home')}}">{{$user->name}}</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></span><span style='width:20px;'></span><span><b>邮箱：</b></span><span><b>{{$user->email}}</b></span></h3>
-			</div>
-		@endforeach
+		<div class="media">
+  <div class="media-left">
+    <a href="{{ URL('blog/'.$user->id.'/home') }}">
+    @if($blog->icon == null)
+		<img src="{{asset('assets/images/website/default_user.jpg')}}" alt="" class="media-object">
+		@else
+		<img src="{{asset('assets/images/icon/'.$blog->icon)}}" alt="" class="media-object">
+		@endif
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading">{{$blog->name}}</h4>
+    {{$blog->introduction}}
+  </div>
+</div>
+	</div>
+	@endforeach
+	</div>
+	<div class="row text-center">
+		{!! $users->render() !!}
+	</div>
+</div>
 
 
 @endsection
